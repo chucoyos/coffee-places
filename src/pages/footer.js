6abcf124e3f4559d9c204f6dac28699d49400bd3
unodev <unodev@outlook.com>
2023-02-26 23:11:48 -0600

    Remove content from index page

--- src/pages/footer.js
+++ src/pages/footer.js
@@ -1,5 +1,5 @@
 export default function Footer() {
   return(
-    <div>Footer</div>
+    <footer>&copy; Uno Dev 2023</footer>
   )
 };
--- src/pages/hello.js
+++
@@ -1,8 +0,0 @@
-import styles from '@/styles/Hello.module.css'
-const Hello = () => {
-  return(
-    <div className='hello'>Hello</div>
-  )
-}
-
-export default Hello;
