2c4246fd95719b86a426b83c7036299ed52eea7d
unodev <unodev@outlook.com>
2023-02-28 02:31:28 -0600

    Add event listener to banner button

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,14 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import Banner from './components/banner'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const handleOnBannerBtnClick = () => {
+    console.log('Btn on click')
+  }
   return (
     <>
       <Head>
@@ -15,9 +18,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div className={styles.title}>
-          <h1>Coffee Connoisseur</h1>
-        </div>
+          <Banner buttonText='View stores nearby' handleOnClick={handleOnBannerBtnClick}/>
       </main>
     </>
   )
